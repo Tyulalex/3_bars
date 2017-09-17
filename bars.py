1e42866b7e93df33dffb7fb2ca778984259b7833
Tatiana Karabut <tyulalex@gmail.com>
2017-09-18 00:23:11 +0300

    bars implementation

--- bars.py
+++ bars.py
@@ -1,21 +1,91 @@
 import json
+import os
+import sys
+import argparse
+from geopy.distance import great_circle
 
 
-def load_data(filepath):
-    pass
+def load_data(input_file):
+    with open(input_file, 'r', encoding='utf-8') as bars_json_file:
+        try:
+            return json.load(bars_json_file)
+        except ValueError:
+            return None
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bars_list):
+    return max(
+        bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bars_list):
+    return min(
+        filter(
+            lambda x: x['properties']['Attributes']['SeatsCount'] != 0,
+            bars_list
+        ), key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bars_list, longitude, latitude):
+    return min(
+        bars_list,
+        key=lambda bar: great_circle(
+            (longitude, latitude), (bar['geometry']['coordinates'])
+        ).meters
+    )
+
+
+def get_formatted_bar_info(bar):
+    return 'это {}, расположенный по адресу: {}'.format(
+        bar.get('properties').get('Attributes').get('Name'),
+        bar.get('properties').get('Attributes').get('Address')
+        )
+
+
+def is_file(raw_file):
+    return os.path.isfile(raw_file)
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--filepath', help="Input file for bars json data (Full path)"
+    )
+    parser.add_argument(
+        '--longtitude', help="Longtitude coordinate"
+    )
+    parser.add_argument(
+        '--latitude', help="latitude coordinate"
+    )
+    args = parser.parse_args()
+    if not args.longtitude or not args.latitude or not args.filepath:
+        print("Please enter your gps coordinates, and path to file, for more info use --help")
+        sys.exit(1)
+    if not is_file(args.filepath):
+        print("Invalid file")
+        sys.exit(1)
+    list_of_bars = load_data(args.filepath).get('features')
+    if not list_of_bars:
+        print('Unsupported type of bar storage, pleae check file')
+        sys.exit(1)
+    biggest_bar = get_biggest_bar(list_of_bars)
+    print(
+        'Самый большой бар {}'.format(
+            get_formatted_bar_info(biggest_bar)
+        )
+     )
+    smallest_bar = get_smallest_bar(list_of_bars)
+    print(
+        'Самый маленький бар {}'.format(
+            get_formatted_bar_info(smallest_bar)
+        )
+    )
+    jps_coordinates = (float(args.longtitude), float(args.latitude))
+    closest_bar = get_closest_bar(list_of_bars, *jps_coordinates)
+    print(
+        'Ближайший бар {}'.format(
+            get_formatted_bar_info(closest_bar)
+        )
+    )
